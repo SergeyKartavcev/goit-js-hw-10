585cf3033dd29a9e37ccdb05d4e98aff37e25315
SergeyKartavcev <102188021+SergeyKartavcev@users.noreply.github.com>
2022-08-21 15:59:43 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './css/styles.css'
 import debounce from 'lodash.debounce'
 import Notiflix from 'notiflix'
-import { fetchCountries } from './fetchCountries'
+import { fetchCountries } from './js/fetchCountries'
+import { renderCountryList } from './js/renderCountryList'
+import { renderCountryInfo } from './js/renderCountryInfo'
 
 const DEBOUNCE_DELAY = 300;
 
@@ -14,7 +16,10 @@ countryInput.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY))
 function onCountryInput() {
   const name = countryInput.value.trim()
   if (name === '') {
-    return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
+     countryList.innerHTML = '',
+      countryInfo.innerHTML = ''
+      Notiflix.Notify.info('please enter same name');
+      return
   }
 
   fetchCountries(name)
@@ -22,10 +27,14 @@ function onCountryInput() {
       countryList.innerHTML = ''
       countryInfo.innerHTML = ''
       if (countries.length === 1) {
-        countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
         countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries))
-      } else if (countries.length >= 10) {
-         Notiflix.Notify.failure('Oops, there is no country with that name')
+        return
+      } 
+      else if (countries.length === 0) {
+        Notiflix.Notify.info('please enter same name');
+     }
+      else if (countries.length >= 10) {
+         Notiflix.Notify.failure("Too many matches found. Please enter a more specific name.")
       } else {
         countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
       }
@@ -33,34 +42,7 @@ function onCountryInput() {
     .catch(Notiflix.Notify.failure('Oops, there is no country with that name'))
 }
 
-function renderCountryList(countries) {
-  const markup = countries
-    .map(({ name, flags }) => {
-      return `
-          <li class="country-list__item">
-              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
-              <h2 class="country-list__name">${name.official}</h2>
-          </li>
-          `
-    })
-    .join('')
-  return markup
-}
 
-function renderCountryInfo(countries) {
-  const markup = countries
-    .map(({ capital, population, languages }) => {
-      return `
-        <ul class="country-info__list">
-            <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
-            <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
-            <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
-        </ul>
-        `
-    })
-    .join('')
-  return markup
-  
-}
+
 
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,5 +4,5 @@ const fields = 'fields=name,capital,population,flags,languages'
 export function fetchCountries(name) {
   return fetch(`${BASE_URL}${name}?${fields}`)
     .then(response => response.json())
-    .catch(error => console.log(error))
+    .catch(error => Notiflix.Notify.error(error.massage))
 }
